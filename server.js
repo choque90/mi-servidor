95d6382c94941cd77307097a32c152609dc73464
choque90 <anderson.choquea@estudiante.ucsm.edu.pe>
2026-06-28 09:47:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log('Servidor corriendo en puerto ' + PORT);
 });
